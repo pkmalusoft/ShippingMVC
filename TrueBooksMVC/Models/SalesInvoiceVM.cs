fbc9509825786112c8c17bcb196ce98c2df7614b
gopakumar <gopakumar.iglowsoft@gmail.com>
2019-09-30 20:25:19 +0530

    salesinvoice design started

---
+++ TrueBooksMVC/Models/SalesInvoiceVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrueBooksMVC.Models
+{
+    public class SalesInvoiceVM 
+    {
+        public string RevenueType { get; set; }
+        public string AcHead { get; set; }
+        public string RevenueCode { get; set; }
+        public int RevenueTypeID { get; set; }
+
+    }
+}
